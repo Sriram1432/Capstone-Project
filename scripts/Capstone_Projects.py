3b9899089ac94d7a6b26a987017c76b135127716
Sriram Gummadi <sriram.gummadi1995@gmail.com>
2020-10-25 12:19:33 +1100

    First Assignment

---
+++ scripts/Capstone_Projects.py
@@ -0,0 +1,8 @@
+
+
+# this notebook will be mainly used for the capstone project and will be updated to github repository
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course!")
